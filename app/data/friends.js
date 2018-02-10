8e827b38f6502ed05ac58a2e7e8974f1b28bf8d7
Tomas Vasquez <baylor@Tomass-MacBook-Pro.local>
2018-02-10 10:03:29 -0600

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,47 @@
+// object for user data
+
+var friends = [
+	{
+		name: "Dwayne",
+		photo: "http://www.indiewire.com/wp-content/uploads/2016/07/9307ba45c3297a6c8b74ed6ecee6cd46ef476335466161576722aabb93c47df2.jpeg?w=780",
+		scores:[3, 4, 1, 3, 2, 3, 2, 2, 4, 5]
+	},
+	{
+		name: "The Man",
+		photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAIA_wDGAAAAAQAAAAAAAAxdAAAAJGVjZDVmMjUyLWNkMDAtNGNkZi1iYTAyLTE2YzNlNmY2ZWMzNw.jpg",
+		scores:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+	},
+	{
+		name: "RG3",
+		photo: "https://i.pinimg.com/236x/00/89/81/0089818e01e0bf67e64d6efa65003b7f.jpg",
+		scores:[3, 3, 1, 3, 2, 1, 2, 1, 4, 1]
+	},
+	{
+	  name: "Terrance Williams",
+	  photo: "https://s3media.247sports.com/Uploads/Assets/271/183/14_5183271.jpg",
+	  scores: [4,2,5,1,3,2,2,1,3,2]
+	},
+	{
+	  name: "Zach Smith",
+	  photo: "https://s3media.247sports.com/Uploads/Assets/96/213/4_5213096.jpeg",
+	  scores: [5,2,2,2,4,1,3,2,5,5]
+	},
+	{
+	  name: "Gia Rodoni",
+	  photo: "https://photos.smugmug.com/Archive/Softball/Arizona-v-Baylor-052617/i-3TVrXmZ/0/004f7d0f/S/052617ua_bay_0808-S.jpg",
+	  scores: [3,3,4,2,2,1,3,2,2,3]
+	},
+	{
+	  name: "Kennedy",
+	  photo: "http://grfx.cstv.com/photos/schools/bay/sports/w-soccer/auto_headshot/12021731.jpeg",
+	  scores: [4,3,4,1,5,2,5,3,1,4]
+	},
+	{
+	  name: "Eva Mendez",
+	  photo: "https://www.famousbirthdays.com/faces/mendes-eva-image.jpg",
+	  scores: [4,4,2,3,2,2,3,2,4,5]
+	}
+
+  ];
+
+module.exports = friends;
